f1f5bb026afce8d75692e9517bbb152d3a99e050
Bekbo <bekbolatabylay@gmail.com>
2019-01-31 13:52:06 +0600

    comment on task2

--- week1/Task2/Task2/Program.cs
+++ week1/Task2/Task2/Program.cs
@@ -6,34 +6,35 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class Student
+    class Student    // new CLass Student
     {
-        string name;
-        string id;
-        int year = 1;
+        string name;  // arguments of the class Student, of type string
+        string id;        // arguments of the class Student, of type string
+        int year = 1;       // arguments of the class Student, of type int 
 
-        public Student(string name, string id)
+        public Student(string name, string id)     //   new parameter from 2 arguments
         {
-            this.name = name;
-            this.id = id;
+            this.name = name;             //   argument name = name
+            this.id = id;                 //    argument id = id
         }
 
-        public void f1()
+        public void f1()        // function f1() for arguments of class Student
         {
-            Console.WriteLine(name + ' ' + id + ' ' + year);
+            Console.WriteLine(name + ' ' + id + ' ' + year);   //Output the arguments
         }
-        public void f2()
+        public void f2() // function f1() for arguments of class Student
         {
-            year++;
+            year++;   // incrementing the year argument
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Student st1 = new Student("Bekbolat", "18BD145319");
-            st1.f1();
-            st1.f2();
+            Student st1 = new Student("Bekbolat", "18BD145319");  // Insert values for st1 element of type of class Student
+            st1.f1();  // do f1() function with st1 element
+            st1.f2();  // do f2() function with st1 element
+            st1.f1();  // do f1() function with st1 element
             Console.ReadKey();
         }
     }
