784c0642bbc7b6713251522c6bd31931f4e67322
Bekbo <bekbolatabylay@gmail.com>
2019-02-28 18:02:01 +0600

    WORK5 play with ser/deser

---
+++ week5/Work/Work/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    public class Student
+    {
+        public string Name;
+        public string SurName;
+        public MarkS MarkS;
+
+        public Student() { }
+        public Student(string Name,string SurName,MarkS MarkS)
+        {
+            this.Name = Name;
+            this.SurName = SurName;
+            this.MarkS = MarkS;
+        }
+        public override string ToString()
+        {
+            return Name + " " + SurName + " " + MarkS;
+        }
+    }
+    public class MarkS
+    {
+        public string Subject;
+        public int MarkSubject;
+        public MarkS() { }
+        public MarkS(string Subject,int MarkSubject)
+        {
+            this.Subject = Subject;
+            this.MarkSubject = MarkSubject;
+        }
+        public string GetLetter()
+        {
+            if (MarkSubject > 94)
+                return Subject + " A";
+            if (MarkSubject > 89)
+                return Subject + "A-";
+            if (MarkSubject > 84)
+                return Subject + "B+";
+            if (MarkSubject > 79)
+                return Subject + "B";
+            if (MarkSubject > 74)
+                return Subject + "B-";
+            if (MarkSubject > 69)
+                return Subject + "C+";
+            if (MarkSubject> 64)
+                return Subject + "C";
+            if (MarkSubject > 59)
+                return Subject + "C-";
+            if (MarkSubject > 54)
+                return Subject + "D+";
+            if (MarkSubject > 49)
+                return Subject + "D";
+            return Subject + "F";
+        }
+        public override string ToString()
+        {
+            return GetLetter();
+        }
+    }
+    public class StudList
+    {
+        public List<Student> Students;
+
+        public StudList()
+        {
+            Students = new List<Student>();
+        }
+        public override string ToString()
+        {
+            return Students + "";
+        }
+    }
+}
---
+++ week5/Work/Work/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Bekbolat";
+            student.SurName = "Ospan";
+            MarkS mark = new MarkS();
+            mark.Subject = "PP2";
+            mark.MarkSubject = 95;
+            student.MarkS = mark;
+
+            Student student2 = new Student();
+            student2.Name = "Abylay";
+            student2.SurName = "Ospan";
+            MarkS mark2 = new MarkS();
+            mark2.Subject = "Calculus";
+            mark2.MarkSubject = 70;
+            student2.MarkS = mark2;
+
+            StudList studList = new StudList();
+            studList.Students.Add(student);
+            studList.Students.Add(student2);
+
+            FileStream fs = new FileStream("students.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(StudList));
+            xs.Serialize(fs, studList);
+            fs.Close();
+
+            FileStream fsopen = new FileStream("students.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xsopen = new XmlSerializer(typeof(StudList));
+            StudList studentOpen = xsopen.Deserialize(fsopen) as StudList;
+            Console.WriteLine(studentOpen);
+            Console.ReadKey();
+        }
+    }
+}
